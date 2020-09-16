f8701173ee8b24d377fe969f77314f79e6fd674b
Vova Abdrakhmanov <369565@gmail.com>
2020-09-16 14:32:00 +0400

    Use sqlalchemy orm

--- lab1.py
+++ lab1.py
@@ -1,38 +1,54 @@
 from weather import TestProvider
 from sqlalchemy import Table, Column, Integer, String, Float, MetaData, create_engine
 from sqlalchemy.sql import select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Weather(Base):
+    __tablename__ = 'weather'
+    id = Column('id', Integer, primary_key=True)
+    date_time = Column('date_time', String)
+    temperature = Column('temperature', Float)
+    wind_speed = Column('wind_speed', Float)
+    visibility = Column('visibility', Float)
+    humidity = Column('humidity', Float)
+    dew = Column('dew', Float)
+
+    def __repr__(self):
+        return "<Weather id=%d date_time='%s' temperature=%f wind_speed=%f visibility=%f humidity=%f dew=%f>" % (
+            self.id,
+            self.date_time,
+            self.temperature,
+            self.wind_speed,
+            self.visibility,
+            self.humidity,
+            self.dew,
+        )
+
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///test.db')
-    metadata = MetaData()
-    weather = Table(
-        'weather',
-        metadata,
-        Column('date_time', String),
-        Column('temperature', Float),
-        Column('wind_speed', Float),
-        Column('visibility', Float),
-        Column('humidity', Float),
-        Column('dew', Float),
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    # Insert a row of data
+    provider = TestProvider()
+    session.add_all(
+        [
+            Weather(
+                date_time=item['datetime'],
+                temperature=item['temp'],
+                wind_speed=item['wspd'],
+                visibility=item['visibility'],
+                humidity=item['humidity'],
+                dew=item['dew'],
+            )
+            for item in provider.get_data('', '', '')
+        ],
     )
-    metadata.create_all(engine)
-    with engine.connect() as connection:
-        # Insert a row of data
-        provider = TestProvider()
-        connection.execute(
-            weather.insert(),
-            [
-                {
-                    'date_time': item['datetime'],
-                    'temperature': item['temp'],
-                    'wind_speed': item['wspd'],
-                    'visibility': item['visibility'],
-                    'humidity': item['humidity'],
-                    'dew': item['dew'],
-                }
-                for item in provider.get_data('', '', '')
-            ],
-        )
 
-        for row in connection.execute(select([weather])):
-            print(row)
+    for weather in session.query(Weather).order_by(Weather.date_time):
+        print(weather)
